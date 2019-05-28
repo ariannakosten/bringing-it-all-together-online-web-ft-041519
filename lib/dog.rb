7dcdfb333eadad2e1d49923a127a7d3d2a00f42a
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-28 21:45:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -104,11 +104,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(hash)
-    @id = hash[:id]
-    @name = hash[:name]
-    @breed = hash[:breed]
-  end
+ 
 
   def self.create_table
     sql = <<-SQL
