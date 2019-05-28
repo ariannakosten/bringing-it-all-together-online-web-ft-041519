205967f12621952ab06cdc19378a3d095f9d437a
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-28 21:36:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -173,19 +173,20 @@ class Dog
   end
 
   # def self.find_or_create_by(name:, breed:)
+     sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+      
+     dog = DB[:conn].execute(sql, name, breed).first
 
-  #   dog = self.find_by_name(name)
-
-  #   if dog.breed != breed
-  #     hash = {
-  #         :name => name,
-  #         :breed => breed }
-  #     new_dog = self.create(hash)
-  #     new_dog
-  #   else
-  #     dog
-  #   end
-  # end
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
+  end 
 
   def self.new_from_db(row)
       hash = {
