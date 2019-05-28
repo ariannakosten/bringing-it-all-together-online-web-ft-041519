aba95a8068e21ea9e207019be8dc0023d9f43276
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-28 21:34:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -211,9 +211,7 @@ def self.find_by_name(name)
   end
 
   def update
-    if self.id == nil
-       self.save
-    else
+
      sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
      DB[:conn].execute(sql, self.name, self.breed, self.id)
    end
